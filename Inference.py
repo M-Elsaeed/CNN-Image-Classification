13edf6e79eff51b702a6d1c42223418af4d67fe3
Mohammed Ehab <m.elsaeed1998@gmail.com>
2021-01-12 16:48:44 +0200

    Commenting and Containerization

--- Inference.py
+++ Inference.py
@@ -1,14 +1,21 @@
 import numpy as np
-import pandas as pd
 import tensorflow as tf
 from tensorflow import keras
 from cv2 import cv2
 import sys
+import platform
 
-# dev, For using GPU for training
-gpus = tf.config.experimental.list_physical_devices(device_type="GPU")
-for gpu in gpus:
-    tf.config.experimental.set_memory_growth(gpu, True)
+rootDir = "/home/16p8160"
+
+# If running on Windows (GPU used, and using relative paths instead of absolute)
+if platform.system() == "Windows":
+    # dev, For using GPU for training
+    gpus = tf.config.experimental.list_physical_devices(device_type="GPU")
+    for gpu in gpus:
+        tf.config.experimental.set_memory_growth(gpu, True)
+    # Use relative path if Windows is used (or not using docker)
+    rootDir = "./project"
+
 
 # For colored priniting
 def printInColor(str):
@@ -22,7 +29,7 @@ model = None
 try:
 
     # Try to load pretrained model, if not found, train the model and save it.
-    model = keras.models.load_model("./trainedmodel.h5")
+    model = keras.models.load_model(f"{rootDir}/trainedmodel.h5")
 
     # Printing the model summary
     # model.summary(print_fn=printInColor)
@@ -38,8 +45,8 @@ try:
 
     # Using the model to get predictions
     prediction = model.predict(x_to_predict)
-    printInColor(f"Predicted Class: {prediction}")
+    printInColor(f"Predicted Class: {[np.where(r==1)[0][0] for r in prediction]}")
 
 except:
-    printInColor("The trained model './trainedmodel.h5' was not found or someother error occured\n")
+    printInColor(f"The trained model '{rootDir}/trainedmodel.h5' was not found or some other error occured\n")
     print(sys.exc_info())
--- Train.py
+++ Train.py
@@ -1,17 +1,51 @@
 from cv2 import cv2
-import pandas as pd
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 from keras.utils import to_categorical
 import os
-
-# dev, For using GPU for training
-gpus = tf.config.experimental.list_physical_devices(device_type="GPU")
-for gpu in gpus:
-    tf.config.experimental.set_memory_growth(gpu, True)
+import platform
 
 
+rootDir = "/home/16p8160"
+
+# If running on Windows (GPU used, and using relative paths instead of absolute)
+if platform.system() == "Windows":
+    # dev, For using GPU for training
+    gpus = tf.config.experimental.list_physical_devices(device_type="GPU")
+    for gpu in gpus:
+        tf.config.experimental.set_memory_growth(gpu, True)
+    # Use relative path if Windows is used (or not using docker)
+    rootDir = "./project"
+
+
+# Takes directory to load images from then saves the read images and labels
+# Inf f"{rootDir}/imgs.npy" andf "{rootDir}/lbls.npy" respectively
+# As a binary file in NumPy .npy format
+#
+# expected directory structure is
+# |root
+# | | Lbl_1 | Img_1.jpg
+# | | Lbl_1 | Img_2.jpg
+# | | Lbl_1 |     .
+# | | Lbl_1 |     .
+# | | Lbl_1 |     .
+# | | Lbl_1 | Img_X.jpg
+# | | Lbl_2 | Img_1.jpg
+# | | Lbl_2 | Img_2.jpg
+# | | Lbl_2 |     .
+# | | Lbl_2 |     .
+# | | Lbl_2 |     .
+# | | Lbl_2 | Img_X.jpg
+# .
+# .
+# .
+# | | Lbl_N | Img_1.jpg
+# | | Lbl_N | Img_2.jpg
+# | | Lbl_N |     .
+# | | Lbl_N |     .
+# | | Lbl_N |     .
+# | | Lbl_N | Img_X.jpg
 def Load_data(directory):
 
     # Initializing Empty Arrays
@@ -43,27 +77,14 @@ def Load_data(directory):
     )
 
     # dev
-    np.save("./imgs", imgs)
-    np.save("./lbls", lbls)
+    np.save(f"{rootDir}/imgs", imgs)
+    np.save(f"{rootDir}/lbls", lbls)
 
     # Return The numpy arrays to caller
     return imgs, lbls
 
 
-def Train():
-
-    # Load images and labels
-    # Expected shapes are
-    # (n, 28, 28, 3) for images array
-    # (n, 1) for labels array
-    # Where n is the number of images, 28*28 is number of pixels
-    # , and 3 is the number of color channels of an RGB image
-    # uncomment next line
-    # x_train, y_train = Load_data("./trainingSet")
-
-    # dev
-    x_train = np.load("./imgs.npy")
-    y_train = np.load("./lbls.npy")
+def Train(x_train=None, y_train=None):
 
     # Shuffling the data
     x_train, y_train = shuffle(x_train, y_train)
@@ -77,7 +98,9 @@ def Train():
     # Building the model
     model = keras.Sequential(
         [
-            keras.layers.experimental.preprocessing.Resizing(28, 28, interpolation='bilinear'),
+            keras.layers.experimental.preprocessing.Resizing(
+                28, 28, interpolation="bilinear"
+            ),
             keras.layers.Conv2D(28, (5, 5), activation="relu", input_shape=(28, 28, 3)),
             keras.layers.MaxPool2D((2, 2)),
             keras.layers.Conv2D(64, (5, 5), activation="relu"),
@@ -99,7 +122,7 @@ def Train():
     )
 
     # BONUS1, tensorboard
-    tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir="./logs")
+    tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=f"{rootDir}/logs")
 
     # training the model
     model.fit(
@@ -115,7 +138,7 @@ def Train():
     model.summary()
 
     # Saving the model
-    model.save("./trainedmodel.h5", overwrite=False, save_format="h5")
+    model.save(f"{rootDir}/trainedmodel.h5", overwrite=False, save_format="h5")
 
 
 def shuffle(a, b):
@@ -123,4 +146,25 @@ def shuffle(a, b):
     return a[p], b[p]
 
 
-Train()
+if __name__ == "__main__":
+    # Load images and labels
+    # Expected shapes are
+    # (n, 28, 28, 3) for images array
+    # (n, 1) for labels array
+    # Where n is the number of images, 28*28 is number of pixels
+    # , and 3 is the number of color channels of an RGB image
+    x_train = None
+    y_train = None
+    try:
+        # dev
+        x_train = np.load(f"{rootDir}/imgs.npy")
+        y_train = np.load(f"{rootDir}/lbls.npy")
+
+        print("DATA LOADED FROM PREREAD NPY FILES")
+    except:
+        print("HEREEEEEE", os.listdir("./"))
+        x_train, y_train = Load_data(f"{rootDir}/trainingSet")
+
+        print("DATA LOADED FROM FILE SYSTEM")
+
+    Train(x_train, y_train)
